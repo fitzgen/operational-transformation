89e483eed3618d3d4eff15b7df44899352311ba9
Nick Fitzgerald <fitzgen@gmail.com>
2011-03-23 12:47:35 -0700

    Piecing lost work back together :( ... making ot.js use the proper AMD format

--- ot.js
+++ ot.js
@@ -1,58 +1,60 @@
-var EventEmitter = require("events").EventEmitter;
+define(['events'], function (events) {
 
-function nop () {}
+    function nop () {}
 
-exports.OT = function (opts) {
-    var store = opts.store || require("./stores/memory-store"),
-        manager = new EventEmitter();
+    return function (opts) {
+        var store = opts.store || require("./stores/memory-store"),
+            manager = new events.EventEmitter();
 
-    manager.newDocument = function (callback) {
-        callback = callback || nop;
-        store.newDocument(function (err, doc) {
-            if ( err ) {
-                this.emit("error", err);
-                return callback(err, null);
-            } else {
-                this.emit("new", doc);
-                return callback(null, doc);
-            }
-        }.bind(this));
-    };
-
-    manager.applyOperations = function (message) {
-        var id = message.id,
-            parentRev = message.rev,
-            ops = message.ops,
-            emit = this.emit.bind(this);
-
-        store.getDocument(id, function (err, doc) {
-            if ( err ) {
-                emit("error", err);
-            } else {
-                if ( rev === doc.rev ) {
-                    try {
-                        doc.doc = apply(ops, doc.doc);
-                    } catch (err) {
-                        emit("error", err);
-                        return;
-                    }
-
-                    doc.rev++;
-                    store.saveDocument(doc, function (err, doc) {
-                        if ( err ) {
-                            // Bad revisions aren't considered an error at this
-                            // level, just ignored.
-                            if ( ! (err instanceof errors.BadRevision) ) {
-                                emit("error", err);
-                            }
-                        } else {
-                            emit("update", doc);
-                        }
-                    });
+        manager.newDocument = function (callback) {
+            callback = callback || nop;
+            store.newDocument(function (err, doc) {
+                if ( err ) {
+                    this.emit("error", err);
+                    return callback(err, null);
+                } else {
+                    this.emit("new", doc);
+                    return callback(null, doc);
                 }
-            }
-        });
+            }.bind(this));
+        };
+
+        manager.applyOperations = function (message) {
+            var id = message.id,
+                parentRev = message.rev,
+                ops = message.ops,
+                emit = this.emit.bind(this);
+
+            store.getDocument(id, function (err, doc) {
+                if ( err ) {
+                    emit("error", err);
+                } else {
+                    if ( rev === doc.rev ) {
+                        try {
+                            doc.doc = apply(ops, doc.doc);
+                        } catch (err) {
+                            emit("error", err);
+                            return;
+                        }
+
+                        doc.rev++;
+                        store.saveDocument(doc, function (err, doc) {
+                            if ( err ) {
+                                // Bad revisions aren't considered an error at this
+                                // level, just ignored.
+                                if ( ! (err instanceof errors.BadRevision) ) {
+                                    emit("error", err);
+                                }
+                            } else {
+                                emit("update", doc);
+                            }
+                        });
+                    }
+                }
+            });
+        };
+
+        return manager;
     };
 
-    return manager;
-};
+});
