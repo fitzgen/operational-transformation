4722bf721bded283bfc3a4a935f65a767dfddabd
Nick Fitzgerald <fitzgen@gmail.com>
2011-03-24 23:33:34 -0700

    Making memory store use AMD

--- stores/memory-store.js
+++ stores/memory-store.js
@@ -1,44 +1,51 @@
-var errors = require("ot/errors");
-var documents = {};
+define(["../errors"], function (errors) {
 
-exports.newDocument = function (callback) {
-    setTimeout(function () {
-        var id;
-        do {
-            id = (new Date()).getTime() + Math.floor(Math.random() * 1000);
-        } while ( id in documents );
-        documents[id] = {
-            id: id,
-            rev: 0,
-            doc: ""
-        };
-        callback(null, documents[id]);
-    }, 10);
-};
+    var documents = {};
 
-exports.getDocument = function (id, callback) {
-    setTimeout(function () {
-        if ( id in documents ) {
-            callback(null, documents[id]);
-        } else {
-            callback(new errors.NoSuchDocument("No document with id = " + id),
-                     null);
+    return {
+
+        newDocument: function (callback) {
+            setTimeout(function () {
+                var id;
+                do {
+                    id = (new Date()).getTime() + Math.floor(Math.random() * 1000);
+                } while ( id in documents );
+                documents[id] = {
+                    id: id,
+                    rev: 0,
+                    doc: ""
+                };
+                callback(null, documents[id]);
+            }, 10);
+        },
+
+        getDocument: function (id, callback) {
+            setTimeout(function () {
+                if ( id in documents ) {
+                    callback(null, documents[id]);
+                } else {
+                    callback(new errors.NoSuchDocument("No document with id = " + id),
+                             null);
+                }
+            }, 10);
+        },
+
+        saveDocument: function (doc, callback) {
+            setTimeout(function () {
+                if ( doc.id in documents ) {
+                    if ( doc.rev === documents[doc.id].rev + 1 ) {
+                        documents[doc.id] = doc;
+                        callback(null, documents[doc.id]);
+                    } else {
+                        callback(new errors.BadRevision("Bad revision"), null);
+                    }
+                } else {
+                    callback(new errors.NoSuchDocument("No document with id = " + doc.id),
+                             null);
+                }
+            }, 10);
         }
-    }, 10);
-};
 
-exports.saveDocument = function (doc, callback) {
-    setTimeout(function () {
-        if ( doc.id in documents ) {
-            if ( doc.rev === documents[doc.id].rev + 1 ) {
-                documents[doc.id] = doc;
-                callback(null, documents[doc.id]);
-            } else {
-                callback(new errors.BadRevision("Bad revision"), null);
-            }
-        } else {
-            callback(new errors.NoSuchDocument("No document with id = " + doc.id),
-                     null);
-        }
-    }, 10);
-};
+    };
+
+});
