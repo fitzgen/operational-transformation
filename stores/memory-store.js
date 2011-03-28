4c4c4dc4dc4c0ad11c6101f11b4c05da02da8556
Nick Fitzgerald <fitzgen@gmail.com>
2011-03-28 00:07:17 -0700

    Need to do deep copies since everything is in memory and side effects will come through to the store

--- stores/memory-store.js
+++ stores/memory-store.js
@@ -1,6 +1,31 @@
+/*jslint onevar: true, undef: true, eqeqeq: true, bitwise: true,
+  newcap: true, immed: true, nomen: false, white: false, plusplus: false,
+  laxbreak: true */
+
+/*global define, setTimeout */
+
 define(["../errors"], function (errors) {
 
     var documents = {};
+
+    function isPrimitive (obj) {
+        return obj === null || typeof obj !== "object";
+    }
+
+    function deepCopy (obj) {
+        var copy, k;
+        if ( isPrimitive(obj) ) {
+            return obj;
+        } else {
+            copy = {};
+            for ( k in obj ) {
+                if ( obj.hasOwnProperty(k) ) {
+                    copy[k] = deepCopy(obj[k]);
+                }
+            }
+            return copy;
+        }
+    }
 
     return {
 
@@ -15,15 +40,19 @@ define(["../errors"], function (errors) {
                     rev: 0,
                     doc: ""
                 };
-                callback(null, documents[id]);
+                callback(null, deepCopy(documents[id]));
             }, 10);
         },
 
         getDocument: function (id, callback) {
+            console.log("inside getDocument");
             setTimeout(function () {
+                console.log("inside getDocument's callback");
                 if ( id in documents ) {
-                    callback(null, documents[id]);
+                    console.log("inside getDocument's callback, success");
+                    callback(null, deepCopy(documents[id]));
                 } else {
+                    console.log("inside getDocument's callback, error");
                     callback(new errors.NoSuchDocument("No document with id = " + id),
                              null);
                 }
@@ -33,9 +62,10 @@ define(["../errors"], function (errors) {
         saveDocument: function (doc, callback) {
             setTimeout(function () {
                 if ( doc.id in documents ) {
+                    console.log(doc.rev, documents[doc.id].rev);
                     if ( doc.rev === documents[doc.id].rev + 1 ) {
-                        documents[doc.id] = doc;
-                        callback(null, documents[doc.id]);
+                        documents[doc.id] = deepCopy(doc);
+                        callback(null, deepCopy(documents[doc.id]));
                     } else {
                         callback(new errors.BadRevision("Bad revision"), null);
                     }
